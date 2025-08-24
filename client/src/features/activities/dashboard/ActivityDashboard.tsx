f4318e9f0e87cc75bd3c81ffde4920f08bd59dc3
juniorbliss49 <ucheeeberechukwu@gmail.com>
2025-08-24 13:41:19 +0100

    end of section

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,14 +1,15 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid } from "@mui/material";
 import ActivityList from "./ActivityList";
+import ActivityFilters from "./ActivityFilters";
 
 export default function ActivityDashboard() {
   return (
     <Grid container spacing={3}>
-      <Grid size={7}>
+      <Grid size={8}>
         <ActivityList />
       </Grid>
-      <Grid size={5}>
-        <Typography>Activity form</Typography>
+      <Grid size={4}>
+        <ActivityFilters />
       </Grid>
     </Grid>
   );
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./app/layout/styles.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -7,7 +8,7 @@ import "@fontsource/roboto/700.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router";
-import { router } from "./app/router/Routes.tsx";
+import { router } from "./app/router/Routes";
 
 const queryClient = new QueryClient();
 
